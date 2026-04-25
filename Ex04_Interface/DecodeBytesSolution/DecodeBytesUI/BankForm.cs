c861ea92dddcf6a205d748f386080523f14ac0ea
cihy999 <cihy999@gmail.com>
2026-04-25 14:11:57 +0800

    feat: 新增範例檔案
    Master C# Interfaces Like a Senior Developer: Software design in C#!
    https://youtu.be/6JD-RdpVauY?si=jDcVcwle2kRnRtVa

---
+++ Ex04_Interface/DecodeBytesSolution/DecodeBytesUI/BankForm.cs
@@ -0,0 +1,43 @@
+﻿using DecodeBytes.Provider;
+
+namespace DecodeBytes.WinForm
+{
+    public partial class BankForm : Form
+    {
+        private readonly IBankProvider _bankProvider;
+        public BankForm(IBankProvider bankProvider)
+        {
+            _bankProvider = bankProvider;
+            InitializeComponent();
+        }
+
+        private void BankForm_Load(object sender, EventArgs e)
+        {
+            this.Text = _bankProvider.ProviderName;
+        }
+
+        private void btn_addToBalance_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(txbx_amount.Text)|| String.IsNullOrEmpty(txbx_cardNumber.Text))
+            {
+                MessageBox.Show("Not allowed empty input");
+                return;
+            }
+            CardNumber cardNumber = new CardNumber(txbx_cardNumber.Text);
+            decimal amount = Convert.ToDecimal(txbx_amount.Text);
+            _bankProvider.AddToBalance(cardNumber, amount);
+        }
+
+        private void btnCheckBalance_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txbx_cardNumber.Text))
+            {
+                MessageBox.Show("Not allowed empty input");
+                return;
+            }
+            CardNumber cardNumber = new CardNumber(txbx_cardNumber.Text);
+           decimal balance = _bankProvider.GetBalance(cardNumber);
+            MessageBox.Show(balance.ToString());
+        }
+    }
+}
---
+++ Ex04_Interface/DecodeBytesSolution/DecodeBytesWPFUI/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace DecodeBytes.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WindowProviderVisualizer _providerVisualizer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _providerVisualizer = new WindowProviderVisualizer(x_mainPanel);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _providerVisualizer.RenderProviders();
+        }
+    }
+}
