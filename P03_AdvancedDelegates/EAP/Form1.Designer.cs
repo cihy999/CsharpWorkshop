38a028395f8b5ad3fe65ba31602a01e922ef8ea7
cihy999 <cihy999@gmail.com>
2026-06-27 11:09:28 +0800

    feat: 新增EAP範例專案

---
+++ P03_AdvancedDelegates/EAP/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace EAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progressBar1 = new ProgressBar();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(49, 88);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(701, 34);
+            progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(295, 254);
+            button1.Name = "button1";
+            button1.Size = new Size(208, 105);
+            button1.TabIndex = 1;
+            button1.Text = "Download";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(381, 149);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 23);
+            label1.TabIndex = 2;
+            label1.Text = "0%";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(progressBar1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+        private Button button1;
+        private Label label1;
+    }
+}
